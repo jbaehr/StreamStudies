a547df3a9cbd808f80f0faa3f232967051b75658
Jonas Bähr <jonas.baehr@web.de>
2018-08-25 21:55:21 +0200

    First testbed and basic wrapper for CryptoStream

---
+++ StreamStudies/SeakableCryptoStream.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace StreamStudies
+{
+    public class SeakableCryptoStream : Stream
+    {
+        private readonly Stream encryptedBaseStream;
+        private readonly SymmetricAlgorithm algorithm;
+        private CryptoStream currentCryptoStream;
+
+        public SeakableCryptoStream(Stream encryptedBaseStream, SymmetricAlgorithm algorithm)
+        {
+            this.encryptedBaseStream = encryptedBaseStream;
+            this.algorithm = algorithm;
+            this.currentCryptoStream = new CryptoStream(
+                encryptedBaseStream,
+                algorithm.CreateDecryptor(),
+                CryptoStreamMode.Read);
+        }
+
+        public override bool CanRead => this.currentCryptoStream.CanRead;
+
+        public override bool CanSeek => this.currentCryptoStream.CanSeek;
+
+        public override bool CanWrite => this.currentCryptoStream.CanWrite;
+
+        public override long Length => this.currentCryptoStream.Length;
+
+        public override long Position
+        {
+            get => this.currentCryptoStream.Position;
+            set => this.currentCryptoStream.Position = value;
+        }
+
+        public override void Flush()
+        {
+            this.currentCryptoStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return this.currentCryptoStream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return this.currentCryptoStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            this.currentCryptoStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            this.currentCryptoStream.Write(buffer, offset, count);
+        }
+    }
+}
---
+++ StreamStudiesTests/SeakableCryptoStreamTest.cs
@@ -0,0 +1,84 @@
+﻿namespace StreamStudiesTests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using NUnit.Framework;
+    using StreamStudies;
+
+    [TestFixture()]
+    public class SeakableCryptoStreamTest
+    {
+        private static readonly int BlockSize = 128;
+        private readonly byte[] InitVecor = Enumerable.Repeat<Byte>(0x00, BlockSize / 8).ToArray();
+        private readonly byte[] Key = Enumerable.Repeat<Byte>(0xff, BlockSize / 8).ToArray();
+
+        [Test()]
+        public void DecryptAllFromBeginning()
+        {
+            var expectedPlainText = this.GetPlainText(4 * BlockSize);
+            Byte[] cypherText;
+            using (var algorithm = this.GetSymmetricAlgorithm())
+            {
+                cypherText = this.GetCypherText(expectedPlainText, algorithm);
+            }
+
+            using (var encryptedBaseStream = new MemoryStream(cypherText))
+            using (var algorithm = this.GetSymmetricAlgorithm())
+            using (var decryptedStream = new SeakableCryptoStream(
+                encryptedBaseStream,
+                algorithm))
+            using (var reader = new BinaryReader(decryptedStream))
+            {
+                var actualPlainText = reader.ReadBytes(expectedPlainText.Length);
+                CollectionAssert.AreEqual(expectedPlainText, actualPlainText);
+            }
+        }
+
+        private SymmetricAlgorithm GetSymmetricAlgorithm()
+        {
+            var algorithm = new AesManaged()
+            {
+                Mode = CipherMode.CBC,
+                BlockSize = BlockSize,
+                IV = InitVecor,
+                Key = Key,
+            };
+
+            return algorithm;
+        }
+
+        private Byte[] GetCypherText(Byte[] plainText, SymmetricAlgorithm algorithm)
+        {
+            using (var memoryStream = new MemoryStream(plainText.Length))
+            {
+                using (var cryptoStream = new CryptoStream(
+                    memoryStream,
+                    algorithm.CreateEncryptor(),
+                    CryptoStreamMode.Write))
+                using (var writer = new BinaryWriter(cryptoStream))
+                {
+                    writer.Write(plainText);
+                }
+
+                return memoryStream.GetBuffer();
+            }
+        }
+
+        private Byte[] GetPlainText(Int32 size)
+        {
+            Assert.GreaterOrEqual(size, 0);
+            Assert.IsTrue(size % 2 == 0, $"{nameof(size)} must be a multiple of 2.");
+
+            var sampleData = new Byte[size];
+            for (Int32 i = 0, c = 0; i < size; c++)
+            {
+                sampleData[i++] = (Byte)c;
+                sampleData[i++] = (Byte)(c >> 8);
+            }
+
+            return sampleData;
+        }
+    }
+}
